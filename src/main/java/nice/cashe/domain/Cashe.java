2737bc0d6ce0b2330580dfe6b65940eb25ed677d
toychip <dlawnsgud427@naver.com>
2023-11-20 13:59:34 +0900

    feat: 10초마다 실행되어 만료된 시간의 객체를 삭제

--- src/main/java/nice/cashe/domain/Cashe.java
+++ src/main/java/nice/cashe/domain/Cashe.java
@@ -1,8 +1,14 @@
 package nice.cashe.domain;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import nice.cashe.domain.cashe_component.Targets;
 import nice.cashe.domain.cashe_component.repository_component.Key;
@@ -13,6 +19,23 @@ public class Cashe {
     private static final ConcurrentHashMap<Key, Targets> repository = new ConcurrentHashMap<>();
 
     public Cashe() {
+    }
+
+    private static final Timer timer = new Timer();
+
+    static {
+        // 10초마다 실행되는 타이머 작업 스케줄링
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                removeExpiredEntries();
+            }
+        }, 0, 10000); // 0초 지연, 10초 간격
+    }
+
+    private static void removeExpiredEntries() {
+        LocalDateTime now = LocalDateTime.now();
+        repository.entrySet().removeIf(entry -> entry.getValue().getUntilTime().isBefore(now));
     }
 
     public Targets get(String inputKey) {
@@ -46,5 +69,10 @@ public class Cashe {
 
     private Targets initTarget(int count, String userInputTime) {
         return new Targets(count, userInputTime);
+    }
+
+    public void remove(String userInputKey) {
+        Key key = initKey(userInputKey);
+        repository.remove(key);
     }
 }
