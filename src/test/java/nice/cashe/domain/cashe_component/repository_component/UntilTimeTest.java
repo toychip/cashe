82375faa2862d3d19c342c9a3de5c302e92d2ef3
toychip <dlawnsgud427@naver.com>
2023-11-20 13:30:56 +0900

    test: UntilTime Test 추가

---
+++ src/test/java/nice/cashe/domain/cashe_component/repository_component/UntilTimeTest.java
@@ -0,0 +1,47 @@
+package nice.cashe.domain.cashe_component.repository_component;
+
+
+import static nice.cashe.domain.ApplicationConstants.INVALID_FORMAT_MESSAGE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDateTime;
+import nice.cashe.domain.exception.InputDateInvalidException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class UntilTimeTest {
+
+    @ParameterizedTest
+    @DisplayName("숫자 포멧에 맞게 생성되는지 검증")
+    @CsvSource("2000.04.27 21:30")
+    void input(String userInputTime){
+        LocalDateTime toTime = LocalDateTime.of(2000, 04, 27, 21, 30);
+        //given
+
+        UntilTime untilTime = new UntilTime(userInputTime);
+        //when
+
+        LocalDateTime timeValue = untilTime.getTimeValue();
+        Assertions.assertEquals(toTime, timeValue);
+        //then
+    }
+
+    @ParameterizedTest
+    @DisplayName("숫자 포멧에 맞지 않으면 InputDateInvalidException 발생")
+    @CsvSource("2000.04.27 21-30")
+    void input2(String userInputTime){
+        // given
+
+        // when && then
+        InputDateInvalidException e = assertThrows(InputDateInvalidException.class,
+                () -> new UntilTime(userInputTime));
+        String errorMessage = e.getMessage();
+
+        assertEquals(INVALID_FORMAT_MESSAGE, errorMessage);
+
+    }
+
+}
